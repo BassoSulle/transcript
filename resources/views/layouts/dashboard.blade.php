7193a3019d2388046f5340ecee0786c956accca9
Testing <bassoelibariki56@gmail.com>
2025-01-19 09:27:57 +0300

    changed the layout of the dashboad.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,6 +13,8 @@
         <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
         <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
+
+
         <!-- Google Fonts -->
         <link href="https://fonts.gstatic.com" rel="preconnect">
         <link
@@ -21,6 +23,7 @@
 
         <!-- Vendor CSS Files -->
         <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        
         <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
